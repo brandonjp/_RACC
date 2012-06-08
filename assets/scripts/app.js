ca211cd254657fd7a01754827481a2c255f77b7a
Brandon Pfeiffer <brandonjp@gmail.com>
2012-06-08 15:49:14 -0400

    / ‘assets/scripts/app.js’

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -3,7 +3,7 @@ jQuery.when(
   jQuery.getJSON('data/lots.json')
 ).done( 
   function(d0, d1) {
-    alert(d0);
+    alert(d0[0]);
     var jqXHR = d0[2]; /* arguments are [ "success", statusText, jqXHR ] */
     if ( /Coffee/.test(jqXHR.responseText) ) {
       alert("First page has 'Whip It' somewhere.");
