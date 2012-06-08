0a1ac0df66cb01cd58b46ec6eda8e47cb11cb286
Brandon Pfeiffer <brandonjp@gmail.com>
2012-06-08 11:23:56 -0400

    + ‘assets/untitled folder/’ / ‘index.js’

--- index.js
+++ index.js
@@ -1,8 +1,24 @@
+if (document.documentElement) document.documentElement.className = 'js';
+
+var RA = {
+  scriptDir: '/assets/scripts/',
+  imgDir:    '/assets/styles/images/'
+};
+
+loadJS(scriptDir+"jquery-1.7.2.min.js", function() {
+
+});
+
+loadJS(scriptDir+"ember-latest.min.js", function() {
+
+});
+
+// fn to add js files to the page
 function loadJS(jsFile, callback) {
-	var jqscript = document.createElement("script");
-	jqscript.setAttribute("type", "text/javascript");
-	jqscript.setAttribute("src", jsFile);
-	document.getElementsByTagName("head")[0].appendChild(jqscript);
+	var jsScript = document.createElement("script");
+	jsScript.setAttribute("type", "text/javascript");
+	jsScript.setAttribute("src", jsFile);
+	document.getElementsByTagName("head")[0].appendChild(jsScript);
 	if (callback != "undefined") {
 		callback();
 	}
