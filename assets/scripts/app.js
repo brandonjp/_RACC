c3632ee87974f90708bd54d08613bfeb91c6f947
Brandon Pfeiffer <brandonjp@gmail.com>
2012-06-08 15:35:50 -0400

    / ‘assets/scripts/app.js’ / ‘index.html’

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,11 +1,13 @@
 jQuery.when(
   jQuery.getJSON('data/cafes.json'), 
   jQuery.getJSON('data/lots.json')
-).done( function(d0, d1) {
-  var jqXHR = d1[2]; /* arguments are [ "success", statusText, jqXHR ] */
-  if ( /Coffee/.test(jqXHR.responseText) ) {
-    alert("First page has 'Whip It' somewhere.");
-  } else { 
-    alert("Whip It not involved."); 
+).done( 
+  function(d0, d1) {
+    var jqXHR = d1[2]; /* arguments are [ "success", statusText, jqXHR ] */
+    if ( /Coffee/.test(jqXHR.responseText) ) {
+      alert("First page has 'Whip It' somewhere.");
+    } else { 
+      alert("Whip It not involved."); 
+    }
   }
-});
+);
